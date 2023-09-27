f085a6d136481966e83c84e8de33b64a2873b1d7
James McNee <james@jamesmcnee.co.uk>
2023-09-27 19:04:26 +0100

    Add pages for each tag (label) and make them clickable

--- .eleventy.js
+++ .eleventy.js
@@ -21,7 +21,27 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy('src/img')
     eleventyConfig.addPassthroughCopy('src/styles')
     eleventyConfig.addPassthroughCopy('src/favicon.ico')
+
     eleventyConfig.addFilter("readableDate", readableDateFilter);
+
+    eleventyConfig.addCollection("postTags", function(collectionApi) {
+        const allPosts = collectionApi.getFilteredByTag("posts")
+
+        const shash = allPosts.reduce((accumulator, current) => {
+            const currentPostsTags = current.data.labels.map(label => label.toLowerCase())
+
+            for (const tag of currentPostsTags) {
+                const previousAccumulatorTagContent = accumulator[tag] ?? []
+                accumulator[tag] = [...previousAccumulatorTagContent, current]
+            }
+
+            return accumulator
+        }, {})
+
+        // console.log(shash)
+
+        return shash
+    });
 
     return {
         dir: {input: 'src', output: '_site'}
